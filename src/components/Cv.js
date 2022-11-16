a9067700447656afbaa9561116d6e77aefbe5bf7
mattand <mjande@umich.edu>
2022-11-15 23:10:28 -0500

    Switch CV to function component

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Name from "./Name";
 import Address from "./Address";
 import Education from "./education/Education";
@@ -6,115 +6,25 @@ import Experience from "./experience/Experience";
 import Skills from "./skills/Skills";
 import "../styles/Cv.css";
 
-class Cv extends Component {
-  constructor(props) {
-    super(props);
+function Cv() {
+  return (
+    <div className="Cv">
+      <Name />
+      <Address />
+      <hr />
 
-    this.state = {
-      firstName: "First Name",
-      lastName: "Last Name",
-      homeAddress: "123 Main St",
-      city: "Cityville, ST",
-      postalCode: "12345",
-      education: [],
-      experience: [],
-      skills: [],
-    };
+      <h2>Education</h2>
+      <Education />
+      <hr />
 
-    this.handleClick = this.handleClick.bind(this);
-    this.handleEnter = this.handleEnter.bind(this);
-    this.handleForm = this.handleForm.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-  }
+      <h2>Work Experience</h2>
+      <Experience />
+      <hr />
 
-  handleClick(e) {
-    const field = e.target.dataset.field;
-
-    this.setState({ [field]: "form" });
-  }
-
-  handleEnter(e) {
-    if (e.key === "Enter") {
-      const field = e.target;
-
-      this.setState({ [field.name]: field.value });
-    }
-  }
-
-  handleForm(form) {
-    const formData = new FormData(form);
-    const field = form.dataset.field;
-    let entry;
-
-    if (field === "education") {
-      entry = {
-        name: formData.get("name"),
-        startYear: formData.get("startYear"),
-        endYear: formData.get("endYear"),
-        degree: formData.get("degree"),
-        gpa: formData.get("gpa"),
-      };
-    } else if (field === "experience") {
-      entry = {
-        role: formData.get("role"),
-        company: formData.get("company"),
-        city: formData.get("city"),
-        startYear: formData.get("startYear"),
-        endYear: formData.get("endYear"),
-      };
-    } else if (field === "skills") {
-      entry = formData.get("name");
-    }
-
-    this.setState({ [field]: [...this.state[field], entry] });
-  }
-
-  handleDelete(e) {
-    const field = e.target.dataset.field;
-    const index = this.state[field].findIndex((entry) => {
-      return entry.degree === e.target.dataset.degree;
-    });
-
-    this.setState({
-      [field]: [
-        ...this.state[field].slice(0, index),
-        ...this.state[field].slice(index + 1),
-      ],
-    });
-  }
-
-  render() {
-    return (
-      <div className="Cv">
-        <Name />
-        <Address />
-        <hr />
-
-        <h2>Education</h2>
-        <Education
-          education={this.state.education}
-          handleForm={this.handleForm}
-          handleDelete={this.handleDelete}
-        />
-        <hr />
-
-        <h2>Work Experience</h2>
-        <Experience
-          experience={this.state.experience}
-          handleForm={this.handleForm}
-          handleDelete={this.handleDelete}
-        />
-        <hr />
-
-        <h2>Skills</h2>
-        <Skills
-          skills={this.state.skills}
-          handleForm={this.handleForm}
-          handleDelete={this.handleDelete}
-        />
-      </div>
-    );
-  }
+      <h2>Skills</h2>
+      <Skills />
+    </div>
+  );
 }
 
 export default Cv;
